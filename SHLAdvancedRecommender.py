97b403367cf26d9dd0f6d19166558d29d4f18202
Sumeet <bt22csd054@iiitn.ac.in>
2025-04-06 22:09:39 +0530

    sqlite fix
    done fix

--- SHLAdvancedRecommender.py
+++ SHLAdvancedRecommender.py
@@ -1,5 +1,10 @@
+# Force modern SQLite
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import re
+import chromadb
 import pandas as pd
 import chromadb
 import requests
